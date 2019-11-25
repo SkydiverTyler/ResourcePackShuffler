223b3d33f720c422691330709d9e4f456f935596
Tyler <skydivertyler@gmail.com>
2019-11-24 23:11:40 -0500

    removed test code from watching a tutorial vid

--- randomtextures.py
+++ randomtextures.py
@@ -1,25 +1,32 @@
-# IF YOU SEE THIS, AND YOU ARE TRYING TO RUN THE RANDOMIZER,
+# IF YOU SEE THIS, AND YOU ARE TRYING TO RUN THE SHUFFLER,
 # YOU PROBABLY DON'T HAVE PYTHON INSTALLED.
-# YOU NEED TO INSTALL PYTHON TO RUN THE RANDOMIZER.
+# YOU WILL NEED TO INSTALL PYTHON TO RUN THE SHUFFLER.
 
 # GET PYTHON AT:
 # https://www.python.org/downloads/
 
-# WHEN SETTING UP PYTHON, MAKE SURE TO CHECK "Add Python __ to PATH"
+# WHEN SETTING UP PYTHON, MAKE SURE TO CHECK "Add Python _._ to PATH"
 # SEE THIS IMAGE FOR MORE INFORMATION:
 # https://datatofish.com/wp-content/uploads/2018/10/0001_add_Python_to_Path.png
 
+# Credits
+# Made by Tyler Jones (SkydiverTyler)
+# https://twitter.com/skydivertyler
+# Please credit me, and link to the github project,
+# if you use this in a YouTube video,
+# or if you redistribute this program.
 
 # Imports
 import os
 from os.path import expanduser
 import subprocess
 import zipfile
-# from tkinter import *
+from tkinter import *
 
 # Define variables
+boolShowGUI = True
 pathNew = "Shuffled packs"
-boolAllowNone = False
+boolAllowEmptyTextures = False
 
 # Create folders if they don't already exist
 try:
@@ -27,19 +34,23 @@ try:
 except:
     print("pathNew already exists")
 
-# # Create the GUI
-# root = Tk()
-# root.configure(background='white')
+# Create the GUI
+if boolShowGUI == True:
 
-# canv = Frame(root, padx=10, pady=10)
-# canv.pack(fill=BOTH)
+    root = Tk()
+    root.configure(background='white')
 
-# lbl1 = Label(canv, text="one", bg="red", fg="white")
-# lbl1.pack()
-# lbl2 = Label(canv, text="two", bg="white", fg="black")
-# lbl2.pack(fill=X)
-# lbl3 = Label(canv, text="three", bg="yellow", fg="black")
-# lbl3.pack(side=LEFT,fill=Y)
+    canv = Frame(root, padx=10, pady=10)
+    canv.pack(fill=BOTH)
 
+    lblPathNew = Label(canv, text="Folder to generate the shuffled packs in")
+    lblPathNew.pack()
 
-# root.mainloop()
+    txt1 = Entry(canv)
+    txt1.pack()
+
+    root.mainloop()
+
+else:
+
+    print("gui not shown")
