dab56dad10be47fb3f973dc2d9dd4eddde3a22bd
Tyler <skydivertyler@gmail.com>
2019-11-29 13:20:56 -0500

    folder for master list - ML not yet created

--- Shuffler.py
+++ Shuffler.py
@@ -19,22 +19,22 @@ from datetime import datetime # for pack naming
 
 # Define variables
 boolShowGUI = False
-boolAllowEmptyTextures = False
-
 dirHere = os.getcwd() # current location of this python script
 dirSrc = "C:/Users/skydi/Desktop/Code/_testfolder" # location of the source resource packs
 dirDest = p.join( dirSrc,"Shuffled Packs" ) # where the shuffled packs will be placed
-dirTemp = p.join( dirSrc,"Generating pack, please wait" ) # temp directory
-dirComp = p.join(dirTemp, "_Assembling pack in here") # folder the shuffled pack will be assembled in
-dirDummy = p.join(dirComp, "assets", "minecraft") # remove when algorithm is created
-
+dirTemp = p.join( dirSrc,"Generating pack - please wait" ) # temp directory
+dirTempList = p.join(dirTemp,"Master file list")
+dirTempExtr = p.join(dirTemp,"Extracted packs")
+dirTempComp = p.join(dirTemp, "Assembling pack in here") # folder the shuffled pack will be assembled in
+dirTempCompDummy = p.join(dirTempComp, "assets", "minecraft") # remove when algorithm is created
 sec = r"\u00A7" # the section symbol, for Minecraft text formatting
 strPackDesc = sec + "7Shuffle your resource packs: " + sec + "3" + sec + "lgit.io/JeXLV"
 intPackFormat = 4
 strPackID = " (" + str(datetime.now()).replace(":","-").replace(".","-") + ")"
 strPackName = "Shuffled Pack" + strPackID
+masterFileList = set()
 
-print("")
+print()
 
 # Functions
 def cDir(thisDir): # create the neccesary folders
@@ -47,67 +47,75 @@ def cDir(thisDir): # create the neccesary folders
 cDir(dirSrc)
 cDir(dirDest)
 cDir(dirTemp)
-cDir(dirComp)
-cDir(dirDummy)
+cDir(dirTempList)
+cDir(dirTempExtr)
+cDir(dirTempComp)
+cDir(dirTempCompDummy)
 
 # Create the GUI
 if boolShowGUI == True:
     root = Tk()
     root.configure(background='white')
-
     canv = Frame(root, padx=10, pady=10)
     canv.pack(fill=BOTH)
-
     lblPathNew = Label(canv, text="Folder to generate the shuffled packs in")
     lblPathNew.pack()
-
     txt1 = Entry(canv)
     txt1.pack()
-
     root.mainloop()
-
 else:
-    print("GUI not shown")
+    print("GUI hidden")
 
 # Extract Folders
     # modified code from https://bit.ly/35ChcSB
 os.chdir(dirSrc) # change working directory to source directory
-for item in os.listdir(dirSrc):
+for item in os.listdir( os.getcwd() ):
     if item.endswith(".zip"):
-        file_name = p.abspath(
-            p.join(dirSrc, item)
-        )
+        file_name = p.join(dirSrc, item)
         z = zipfile.ZipFile(file_name)
-        print("Extracting " + item)
-        z.extractall(
-            p.join(dirTemp, p.splitext(item)[0])
+        z.extractall(   # extract all zips to temp dir
+            p.join(dirTempExtr, p.splitext(item)[0])
         )
+        z.extractall(dirTempList,
+            members=(member for member in z.namelist() if not member.endswith('.mcmeta')) # from https://bit.ly/35JlqYs
+        )
+        print("Extracted [" + item + "]")
         z.close()
-        #shutil.rmtree(dirTemp)
+
+# Get a list of all file names
+os.chdir(dirTempList)
+for folderName, subfolders, filenames in os.walk( dirTempComp ): # Location of the src assets and pack
+    for filename in filenames:
+        if filename.endswith(".zip") == False:
+            filePath = p.join(folderName, filename) #create complete filepath of file in directory
+            z.write( p.relpath( filePath ) ) # add file to zip
+            print("Zipped [" + p.relpath(filePath) + "]")
+
+
+# Actually shuffle the packs
 
 # Write the Minecraft Meta file
-mcm = p.join(dirComp,"pack.mcmeta")
+mcm = p.join(dirTempComp,"pack.mcmeta")
 t = open(mcm,"w")
 t.write( '{"pack": {"pack_format":' + str(intPackFormat) + ',"description": "' + strPackDesc + '"} }' )
 t.close()
 
-dummyfile = p.join(dirDummy,"test.txt")
+dummyfile = p.join(dirTempCompDummy,"test.txt")
 t = open(dummyfile,"w")
 t.write("lmao")
 t.close()
 
 # Create final resource pack zip folder
-print("")
-os.chdir(dirComp) # change working dir to compilation dir
+os.chdir(dirTempComp) # change working dir to compilation dir
 with zipfile.ZipFile( p.join( dirDest, strPackName + '.zip') , 'w') as z: # Location of the zip file
-    for folderName, subfolders, filenames in os.walk( dirComp ): # Location of the src assets and pack
+    for folderName, subfolders, filenames in os.walk( dirTempComp ): # Location of the src assets and pack
         for filename in filenames:
             if filename.endswith(".zip") == False:
-                filePath = os.path.join(folderName, filename) #create complete filepath of file in directory
+                filePath = p.join(folderName, filename) #create complete filepath of file in directory
                 z.write( p.relpath( filePath ) ) # add file to zip
-                print("File [" + p.relpath(filePath) + "] zipped" )
+                print("Zipped [" + p.relpath(filePath) + "]")
 
 # Let the user know the pack has been generated
-print()
-print(strPackName + " successfully created!")
-print()
+print("")
+print("Success! [" +strPackName + "] created.")
+print("")
